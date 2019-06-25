58489df9bfb0d2f538388cad4db04d916fbeb833
Tabitha <tabitha@davinci.ece.uvic.ca>
2019-06-24 18:07:33 -0700

    saved histograms for every 5 frames, with 4 sample videos

--- NewHogBased/scratch.py
+++ NewHogBased/scratch.py
@@ -1,11 +1,13 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import math
 
 
 cap = cv2.VideoCapture('/home/tabitha/Desktop/automatic-detection-of-fish-behaviour/good_vids/'
-                       'BC_POD1_PTILTVIDEO_20110519T091755.000Z_1.ogg')
+                       'BC_POD1_PTILTVIDEO_20110522T173147.000Z_2.ogg')
 
+'''
 # params for ShiTomasi corner detection
 feature_params = dict( maxCorners = 100,
                        qualityLevel = 0.3,
@@ -16,6 +18,7 @@ feature_params = dict( maxCorners = 100,
 lk_params = dict( winSize  = (15,15),
                   maxLevel = 2,
                   criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+'''
 
 # Create some random colors
 color = np.random.randint(0,255,(100,3))
@@ -24,6 +27,7 @@ if (cap.isOpened() == False):
     print("Error opening video stream or file")
 
 # fig = plt.figure()
+#fig, figarray = plt.subplots(2, 1)
 
 # Take first frame
 ret, frame1 = cap.read()
@@ -34,10 +38,16 @@ if ret:
 
 # add a count flag for the number
 frameCount = 0
-numberOfFrames = 5
-print(frame1.shape)
-frameFlowArray = np.zeros(5 * (frame1.shape[0], frame1.shape[1], 2), dtype=np.float32)
-print('flow array shape: ', frameFlowArray.shape)
+numberOfFrames = 3
+numBins = 16
+myRange = np.arange(-math.pi, math.pi*((numBins/2+1)/(numBins/2)), math.pi/(numBins/2))
+summedHist = np.zeros((numBins,))
+savedPlotCount = 0
+
+# Cannot specify a large array as not enough memory
+# frameFlowArray = np.zeros(5 * (frame1.shape[0], frame1.shape[1], 2), dtype=np.float32)
+
+#print('flow array shape: ', frameFlowArray.shape)
 
 while(cap.isOpened()):
     ret, frame2 = cap.read()
@@ -47,13 +57,28 @@ while(cap.isOpened()):
         mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
         hsv[..., 0] = ang * 180 / np.pi / 2
         hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+        frameHist, bins = np.histogram(ang, bins=myRange, weights=mag, density=True)
+
         bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
-        # add the flow arrays to a big array
+        # add the histograms
+        if frameCount is numberOfFrames:
+            # save histograms to file
+            figNameString = '/home/tabitha/Desktop/automatic-detection-of-fish-behaviour/savedHistograms/' \
+                            + '{0:08}'.format(savedPlotCount) + '.png'
+            plt.subplot(2, 1, 1)
+            plt.bar(bins[:-1], summedHist, align='edge', width=math.pi / (numBins / 2))
+            plt.savefig(figNameString)
+            plt.clf()
 
-        # processing step
+            savedPlotCount += 1
+            frameCount = 0
+            summedHist = np.zeros((numBins,))
+            print('saved figure', savedPlotCount)
 
+        summedHist += frameHist
 
+        plt.subplot(2, 1, 2)
         plt.imshow(bgr)
         plt.pause(0.001)
 

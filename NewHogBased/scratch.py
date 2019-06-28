f0d319bf2d39ed6f57ecc6222a248a40bce6556c
Tabitha <tabitha@davinci.ece.uvic.ca>
2019-06-28 15:05:27 -0700

    adding new histograms for (3) and some slight changes in code in scratch file

--- NewHogBased/scratch.py
+++ NewHogBased/scratch.py
@@ -3,9 +3,19 @@ import cv2
 import matplotlib.pyplot as plt
 import math
 
+'''
+# can do these later
+def divide_frame():
+
+def get_histogram():
+
+def get_hsv_image():
+'''
+
+
 
 cap = cv2.VideoCapture('/home/tabitha/Desktop/automatic-detection-of-fish-behaviour/good_vids/'
-                       'BC_POD1_PTILTVIDEO_20110522T173147.000Z_2.ogg')
+                       'BC_POD1_PTILTVIDEO_20110522T114342.000Z_3.ogg')
 
 '''
 # params for ShiTomasi corner detection
@@ -44,8 +54,8 @@ myRange = np.arange(0, 2 * math.pi + (2*math.pi/numBins), 2*math.pi/numBins)
 #print(myRange)
 summedHist = np.zeros((numBins,))
 savedPlotCount = 0
-gridDivision = 3
-
+gridDivisionW = 8
+gridDivisionH = 6
 # Cannot specify a large array as not enough memory
 # frameFlowArray = np.zeros(5 * (frame1.shape[0], frame1.shape[1], 2), dtype=np.float32)
 
@@ -56,6 +66,9 @@ while(cap.isOpened()):
     if ret:
         next = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
+        # here is where divide the frame into subregions
+
+
         #next = cv2.adaptiveThreshold(next, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
         # adding a little bit of gaussian to try and remove noise
@@ -63,7 +76,7 @@ while(cap.isOpened()):
 
         # adding otsu's method and morphological operations
         #_, next = cv2.threshold(next, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        # erosion
+
         #erosionKernel = np.ones((5, 5), np.uint8)
         erosionKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))
         dilationKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))
@@ -92,22 +105,24 @@ while(cap.isOpened()):
         hsv[..., 0] = ang * 180 / np.pi / 2
         bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
+
         # draw a 3x3 grid onto the image
         # get the top points and the left points
-        heightDivision, widthDivision = np.floor(frame2.shape[0] / gridDivision).astype(np.int), np.floor(
-            frame2.shape[1] / gridDivision).astype(np.int)
-        topPoints = [(i * widthDivision, 0) for i in range(1, gridDivision)]
-        bottomPoints = [(i * widthDivision, frame2.shape[0]-1) for i in range(1, gridDivision)]
-        leftPoints = [(0, i * heightDivision) for i in range(1, gridDivision)]
-        rightPoints = [(frame2.shape[1]-1, i * heightDivision) for i in range(1, gridDivision)]
+        heightDivision, widthDivision = np.floor(frame2.shape[0] / gridDivisionH).astype(np.int), np.floor(
+            frame2.shape[1] / gridDivisionW).astype(np.int)
+        topPoints = [(i * widthDivision, 0) for i in range(1, gridDivisionW)]
+        bottomPoints = [(i * widthDivision, frame2.shape[0]-1) for i in range(1, gridDivisionW)]
+        leftPoints = [(0, i * heightDivision) for i in range(1, gridDivisionH)]
+        rightPoints = [(frame2.shape[1]-1, i * heightDivision) for i in range(1, gridDivisionH)]
         # draw a line
         for i in range(len(topPoints)):
-            print(topPoints[i])
+            #print(topPoints[i])
             cv2.line(bgr, topPoints[i], bottomPoints[i], (0, 255, 0), thickness=3, lineType=8, shift=0)
+        for i in range(len(leftPoints)):
             cv2.line(bgr, leftPoints[i], rightPoints[i], (0, 255, 0), thickness=3, lineType=8, shift=0)
 
+
         # add the histograms
-        # TODO - the frame and the histogram may be one frame off!
         if frameCount is numberOfFrames:
             # save histograms to file
             figNameString = '/home/tabitha/Desktop/automatic-detection-of-fish-behaviour/savedHistograms/' \

fixed the averaging issue

NewHogBased/scratch.py
@@ -13,21 +13,21 @@ from matplotlib.offsetbox import AnchoredText
 # -----------------------------------PARAMETERS-----------------------------------------------
 
 
-num_divisionsW = 5
-num_divisionsH = 5
+num_divisionsW = 2
+num_divisionsH = 2
 
 # tracked_region_list = range(1, num_divisionsW * num_divisionsH + 1)
-tracked_region_list = [11, 12, 13, 17]
+tracked_region_list = [1, 2, 3, 4]
 
 ylim_max = 60
 
 numberOfFrames = 3
 numBins = 16
 
-startleFrame = 36
+startleFrame = 28
 extraStartle = False
 
-videoTitle = 'BC_POD1_PTILTVIDEO_20110625T183602.000Z_1.ogg'
+videoTitle = 'BC_POD1_PTILTVIDEO_20110616T171904.000Z_2.ogg'
 videoString = ''.join(('/home/tabitha/Desktop/automatic-detection-of-fish-behaviour/good_vids/sablefish/', videoTitle))
 
 saveHistograms = True
@@ -52,7 +52,7 @@ hist_layout = plt.GridSpec(2, 2)
 
 frameCount = 0
 savedPlotCount = 0
-summedHist = np.zeros((numBins,))
+frameSummedHist = np.zeros((numBins,))
 
 non_displayed_region = [i for i in range(1, num_divisionsW * num_divisionsH + 1) if (i not in tracked_region_list)]
 myRange = np.arange(0, 2 * math.pi + (2 * math.pi / numBins), 2 * math.pi / numBins)
@@ -93,8 +93,8 @@ def get_next_window(index, roi_list, next_window_list):
 def get_roi(frame, num_divisionsW, num_divisionsH):
     gridDivisionW = np.floor(frame.shape[1] / num_divisionsW).astype(np.int)
     gridDivisionH = np.floor(frame.shape[0] / num_divisionsH).astype(np.int)
-    roi_list = [(frame[y*gridDivisionH:(y+1)*gridDivisionH, x*gridDivisionW:(x+1)*gridDivisionW]) for x in range(num_divisionsW)
-                for y in range(num_divisionsH)]
+    roi_list = [(frame[y*gridDivisionH:(y+1)*gridDivisionH, x*gridDivisionW:(x+1)*gridDivisionW])
+                for x in range(num_divisionsW) for y in range(num_divisionsH)]
     return roi_list
 
 
@@ -143,35 +143,39 @@ def draw_grid(num_divisionsH, num_divisionsW, bgr):
 
 
 def plot_histogram(frameCount, savedPlotCount, frameHist, myRange, numBins, save, hist_layout):
-    # save histograms to file
-    figPath = '/home/tabitha/Desktop/automatic-detection-of-fish-behaviour/savedHistograms/'
-    if saveToFolder:
-        figNameString = figPath + 'histograms/' + videoTitle + '/{0:08}'.format(savedPlotCount) + '.png'
-    else:
-        figNameString = figPath + '/{0:08}'.format(savedPlotCount) + '.png'
+    if frameCount is numberOfFrames:
+        # save histograms to file
+        figPath = '/home/tabitha/Desktop/automatic-detection-of-fish-behaviour/savedHistograms/'
+        if saveToFolder:
+            figNameString = figPath + 'histograms/' + videoTitle + '/{0:08}'.format(savedPlotCount) + '.png'
+        else:
+            figNameString = figPath + '/{0:08}'.format(savedPlotCount) + '.png'
 
-    plt.figure("main figure")
-    plt.subplot(hist_layout[0, 0:])
-    plt.ylim(0, ylim_max)
-    plt.bar(myRange[:-1], frameHist / hist_scaling_factor, align='edge', width=2 * math.pi / numBins)
+        plt.figure("main figure")
+        plt.subplot(hist_layout[0, 0:])
+        plt.ylim(0, ylim_max)
+        plt.bar(myRange[:-1], frameHist / hist_scaling_factor, align='edge', width=2 * math.pi / numBins)
+        #plt.bar(myRange[:-1], frameHist, align='edge', width=2 * math.pi / numBins)
 
-    # plot visualisation stuff
-    plt.title('Histogram of Optical Flow')
-    plt.ylabel('Scaled Vector Histogram')
-    plt.xlabel('Degrees (rad)')
-    plt.grid(True)
-    plt.tight_layout()
+        # plot visualisation stuff
+        plt.title('Histogram of Optical Flow')
+        plt.ylabel('Scaled Vector Histogram')
+        plt.xlabel('Degrees (rad)')
+        plt.grid(True)
+        plt.tight_layout()
 
-    if save is True:
-        if not os.path.exists(''.join((figPath, videoTitle))):
-            os.mkdir(''.join((figPath, videoTitle)))
-        plt.savefig(figNameString)
-        plt.clf()
+        if save is True:
+            if not os.path.exists(''.join((figPath, videoTitle))):
+                os.mkdir(''.join((figPath, videoTitle)))
+            plt.savefig(figNameString)
+            plt.clf()
 
-    savedPlotCount += 1
+        savedPlotCount += 1
 
-    if save is True:
-        print('saved figure', savedPlotCount)
+        if save is True:
+            print('saved figure', savedPlotCount)
+
+        frameCount = 0
 
     return frameCount, savedPlotCount
 
@@ -205,9 +209,6 @@ skew_list = []
 kurtosis_list = []
 max_list = []
 
-summed_tracked_mag = []
-summed_tracked_ang = []
-
 # Take first frame
 ret, frame1 = cap.read()
 if ret:
@@ -220,6 +221,9 @@ if ret:
     frame_hsv = np.zeros((pixels_height, pixels_width, 3))
     hsv[..., 1] = 255
     frame_hsv[..., 1] = 255
+
+    summed_tracked_ang = np.zeros((len(tracked_region_list), pixels_height, pixels_width))
+    summed_tracked_mag = np.zeros((len(tracked_region_list), pixels_height, pixels_width))
 
 while cap.isOpened():
     ret, frame2 = cap.read()
@@ -251,7 +255,7 @@ while cap.isOpened():
                      #for i in range(len(tracked_region_list))]
 
         # divide optical flow into grid lists
-        mag_list = get_roi(hsv[..., 1], num_divisionsW, num_divisionsH)
+        mag_list = get_roi(hsv[..., 2], num_divisionsW, num_divisionsH)
         ang_list = get_roi(myAngles, num_divisionsW, num_divisionsH)
 
         # take only the regions of interest
@@ -261,23 +265,23 @@ while cap.isOpened():
         # average out the list
         if frameCount is numberOfFrames:
             # average the lists
-            summed_tracked_mag = tracked_mag_list * 1.0 / numberOfFrames
-            summed_tracked_ang = tracked_ang_list * 1.0 / numberOfFrames
+            #summed_tracked_mag = tracked_mag_list * 1.0 / numberOfFrames
+            #summed_tracked_ang = tracked_ang_list * 1.0 / numberOfFrames
+
+            # only take the histogram of averaged vectors
+            frameHist = [np.histogram(summed_tracked_ang[i] / numberOfFrames, bins=myRange,
+                                      weights=summed_tracked_mag[i] / numberOfFrames, density=False)[0]
+                         for i in range(len(tracked_region_list))]
+
+            # sum up the histograms per frame
+            frameSummedHist = np.sum(frameHist, axis=0)
 
             # reset the sum back to zero
-            summed_tracked_mag.clear()
-            summed_tracked_ang.clear()
-            frameCount = 0
+            summed_tracked_ang = np.zeros((len(tracked_region_list), pixels_height, pixels_width))
+            summed_tracked_mag = np.zeros((len(tracked_region_list), pixels_height, pixels_width))
 
-        summed_tracked_mag += tracked_mag_list
-        summed_tracked_ang += tracked_ang_list
-
-        # only take the histogram of averaged vectors
-        frameHist, _ = [np.histogram(summed_tracked_ang[i], bins=myRange, weights=summed_tracked_mag[i], density=False)
-                        for i in range(len(tracked_region_list))]
-
-        # sum up the histograms per frame
-        frameSummedHist = np.sum(frameHist, axis=0)
+        summed_tracked_mag += np.array(tracked_mag_list)
+        summed_tracked_ang += np.array(tracked_ang_list)
 
         bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
@@ -301,18 +305,16 @@ while cap.isOpened():
         plt.imshow(frame2)
         plt.pause(0.001)
 
-        # add the histograms
+        # plot the histograms
         frameCount, savedPlotCount = plot_histogram(frameCount, savedPlotCount, frameSummedHist, myRange, numBins,
                                                     saveHistograms, hist_layout)
 
-
-
         # calculate and save the histogram metrics
-        hist_skew = skew(summedHist, bias=True)
+        hist_skew = skew(frameSummedHist, bias=True)
         skew_list.append(hist_skew)
-        hist_kurtosis = kurtosis(summedHist, bias=True, fisher=False)
+        hist_kurtosis = kurtosis(frameSummedHist, bias=True, fisher=False)
         kurtosis_list.append(hist_kurtosis)
-        hist_max = np.amax(summedHist)
+        hist_max = np.amax(frameSummedHist)
         max_list.append(hist_max)
 
         hist_text = '\n'.join(('skew=%.2f' % (hist_skew, ), 'kurtosis=%.2f' % (hist_kurtosis, ),
@@ -333,7 +335,7 @@ while cap.isOpened():
 plt.figure("data")
 
 # print('savedplotcount: ', savedPlotCount, 'hist_skew count: ', len(skew_list))
-plt.subplot(4, 1, 1, zorder=1)
+plt.subplot(3, 1, 1, zorder=1)
 s1 = plt.axvline(x=startleFrame, ymin=-3.2, ymax=1, label='startle', c='red', zorder=20, clip_on=False)
 plt.text(startleFrame, max(skew_list) + 0.3, "Startle", color='red')
 if extraStartle:
@@ -341,16 +343,16 @@ if extraStartle:
     plt.text(extraStartle, max(skew_list) + 0.3, "Startle", color='red')
 plt.title('Skew')
 p1 = plt.plot(range(len(skew_list)), skew_list, c='blue', zorder=2)
-plt.subplot(4, 1, 2, zorder=-1)
+plt.subplot(3, 1, 2, zorder=-1)
 plt.title('Kurtosis')
 p2 = plt.plot(range(len(kurtosis_list)), kurtosis_list, c='blue', zorder=2)
-plt.subplot(4, 1, 3, zorder=-1)
+plt.subplot(3, 1, 3, zorder=-1)
 plt.title('Max')
 p3 = plt.plot(range(len(max_list)), max_list, c='blue', zorder=2)
-plt.subplot(4, 1, 4, zorder=-1)
-plt.title('Kurtosis / Skew')
-test = [float(ai)/bi for ai, bi in zip(kurtosis_list, skew_list)]
-p3 = plt.plot(range(len(max_list)), test, c='blue', zorder=2)
+#plt.subplot(3, 1, 4, zorder=-1)
+#plt.title('Kurtosis / Skew')
+#test = [float(ai)/float(bi) for ai, bi in zip(kurtosis_list, skew_list)]
+#p3 = plt.plot(range(len(max_list)), test, c='blue', zorder=2)
 
 plt.xlabel('Saved Plot Frame')
 plt.subplots_adjust(hspace=0.6)
e3fbd09f3a0e2cb5e6f53c2648116760ba61e58e
Tabitha <tabitha@davinci.ece.uvic.ca>
2019-07-22 17:11:37 -0700

    changed some stats stuff, now has included weights and can save matlab matrices

--- NewHogBased/stats.py
+++ NewHogBased/stats.py
@@ -14,8 +14,33 @@ def my_skew(data):
     moment2 = calc_moment(data, 2)
     return moment3 / (moment2 ** (3 / 2))
 
+
 # using the kurtosis excess formula that should be centered around 0
 def my_kurtosis(data):
     moment4 = calc_moment(data, 4)
     moment2 = calc_moment(data, 2)
     return moment4 / (moment2 ** 2)-3
+
+
+def find_weighted_mean(data, weights):
+    my_product = [xi * wi for xi, wi in zip(data, weights)]
+    return sum(my_product) / sum(weights)
+
+def find_weighted_moment(data, weights, deg):
+    my_mean = find_weighted_mean(data, weights)
+    if deg is 2:
+        sp = 1
+    else:
+        sp = find_weighted_moment(data, weights, 2)
+    my_dist = [(xi - my_mean) / sp for xi in data]
+    my_product = [wi * (di ** deg) for wi, di in zip(weights, my_dist)]
+    return sum(my_product) / sum(weights)
+
+
+def find_weighted_skew(data, weights):
+    return find_weighted_moment(data, weights, 3)
+
+
+def find_weighted_kurtosis(data, weights):
+    return find_weighted_moment(data, weights, 4) - 3
+
--- NewHogBased/stats_test.py
+++ NewHogBased/stats_test.py
@@ -1,4 +1,5 @@
 import stats
+import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import skew, kurtosis
@@ -7,9 +8,13 @@ from scipy.stats import skew, kurtosis
 # get an array of normally sampled data
 mu, sigma = 0, 1
 scale = 30
-#s = np.random.standard_cauchy(mu, sigma, 1000000)
-s = np.random.weibull(1.5, 1000000)
+#s = np.random.standard_cauchy(mu, sigma, 1000)
+#s = np.random.standard_cauchy(1000)
+s = np.random.weibull(1.5, 1000)
 #s = s[(s>-10) & (s<10)]
+#s = np.random.laplace(mu, sigma, 1000)
+#s = np.random.normal(mu, sigma, 1000)
+scipy.io.savemat('/home/tabitha/Desktop/laplace.mat', mdict={'arr': s})
 
 # verify the mean and variance ?
 print(abs(mu - np.mean(s)) < 0.01)
@@ -17,7 +22,7 @@ print(abs(sigma - np.std(s, ddof=1)) < 0.01)
 
 count, bins = np.histogram(s, 300, density=False)
 plt.bar(bins[:-1], count, align='edge')
-print('skew: ', stats.my_skew(count), 'kurtosis: ', stats.my_kurtosis(count))
+print('skew: ', stats.my_skew(s), 'kurtosis: ', stats.my_kurtosis(s))
 print('Scipy: skew: ', skew(count), 'kurtosis: ', kurtosis(count))
 
 plt.pause(10)
